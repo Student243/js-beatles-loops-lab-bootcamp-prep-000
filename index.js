d748bd1202d261ec4f07406dabdbda6913ddb7ba
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-06-13 23:38:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+var array = []
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+var i;
+var text
+var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+var numbers = []
+function theBeatlesPlay(musicians, instruments) {
+  for (i = 0; i < musicians.length; i++) {
+    text = musicians [i] + " plays " + instruments[i] 
+    array.push(text)
+}
+ return array
+}
+function johnLennonFacts(facts) {
+    var new_facts = []
+  for (i = 0; i < facts.length; i++) {
+    text = facts [i] + "!!!"
+    new_facts.push(text)
+  }
+  return new_facts
+}
+function iLoveTheBeatles(number) {
+    function maybeTrue() {
+  return Math.random() >=15
+}
+  do {
+    number.pop( "I love the Beatles")
+  } while (maybeTrue()||number.length > 0)
+  return number
+}
